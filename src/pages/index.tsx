00f8e8b00908b9467037af4f6314bf0e07ec7ee4
kirill-vypirovskyi <kirill.vypirovsky@gmail.com>
2023-04-19 19:22:40 +0300

    auth showcase removed from index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,27 +23,3 @@ const Home: NextPage = () => {
 };
 
 export default Home;
-
-const AuthShowcase: React.FC = () => {
-  const { data: sessionData } = useSession();
-
-  const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined }
-  );
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
-      <button
-        className="rounded-full bg-primary/10 px-10 py-3 font-semibold no-underline transition hover:bg-primary/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
-  );
-};
