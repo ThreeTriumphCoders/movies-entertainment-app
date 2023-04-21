3663006a30239ae4c9d6948b6e3fa76c758a894b
Anastasiia Kuznetsova <an.stsii.k@gmail.com>
2023-04-21 15:08:25 +0200

    add scroll to top buttom to layout

---
+++ src/components/ScrollUpButton.tsx
@@ -0,0 +1,36 @@
+import classNames from "classnames";
+import { useEffect, useState } from "react";
+
+export const ScrollUpButton = () => {
+  const [isEnabled, setIsEnabled] = useState(false);
+  
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setIsEnabled(true);
+      } else {
+        setIsEnabled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <button 
+      type="button"
+      className={classNames(
+        "fixed bottom-10 right-10 bg-arrow-up w-12 h-12 hover:bg-arrow-hover transition-all",
+        {
+          'hidden': !isEnabled, 
+        }
+      )}
+      onClick={() => window.scrollTo(0, 0)}
+    >
+    </button>
+  );
+};
