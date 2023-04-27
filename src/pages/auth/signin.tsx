05ae5d9d13f1b47fdc4c1e3692591bca88fe9236
Anastasiia Kuznetsova <an.stsii.k@gmail.com>
2023-04-27 12:51:24 +0200

    fixed design & adaptive

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -6,6 +6,8 @@ import Image from 'next/image';
 import { signIn } from 'next-auth/react';
 import { useState } from 'react';
 import { Loader } from '~/components/Loader';
+import { SvgIcon } from '~/components/SvgIcon';
+import { IconName, getIconByName } from '~/utils/getIconByName';
 
 const SignIn = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -22,36 +24,41 @@ const SignIn = () => {
   }
 
   return (
-    <section className="flex justify-center items-center bg-dark h-screen">
+    <section className="flex flex-col gap-20 justify-center items-center bg-dark h-screen">
       {isLoading
         ? <Loader />
         : (
-          <div className="flex flex-col bg-semi-dark px-10 pt-12 pb-24 rounded-lg w-1/4 font-body text-light">
-            <h1 className="font-light text-3xl text-center mb-12">
-              Login
-            </h1>
+          <>
+            <SvgIcon className='fill-primary w-8 h-8'>
+              {getIconByName(IconName.MOVIE)}
+            </SvgIcon>
 
-            <div className="flex flex-col gap-y-6">
-              <button 
-                type="button" 
-                className="flex gap-x-5 justify-center items-center border border-grey rounded-lg w-full py-3 hover:bg-primary hover:border-primary hover:text-dark transition-all"
-                onClick={() => void handleSignIn('google')}
-              >
-                <Image src={google} alt="google" width="28" height="28" />
-                Login with Google
-              </button>
+            <div className="flex flex-col bg-semi-dark px-8 pt-8 pb-28 rounded-3xl w-4/5 sm:w-1/2 lg:w-1/3 xl:w-1/4 font-body text-light">
+              <h1 className="font-body text-3xl text-center mb-16">
+                Login
+              </h1>
 
-              <button 
-                type="button" 
-                className="flex gap-x-5 justify-center items-center border border-grey rounded-lg w-full py-3 hover:bg-primary hover:border-primary hover:text-dark transition-all"
-                onClick={() => void handleSignIn('github')}
-              >
-              <Image src={github} alt="google" width="28" height="28" />
-                Login with GitHub
-              </button>
+              <div className="flex flex-col gap-y-6">
+                <button 
+                  type="button" 
+                  className="flex gap-x-5 justify-center items-center font-light border border-grey rounded-lg w-full py-4 hover:bg-primary hover:border-primary hover:text-dark transition-all"
+                  onClick={() => void handleSignIn('google')}
+                >
+                  <Image src={google} alt="google" width="28" height="28" />
+                  Login with Google
+                </button>
+
+                <button 
+                  type="button" 
+                  className="flex gap-x-5 justify-center items-center font-light border border-grey rounded-lg w-full py-4 hover:bg-primary hover:border-primary hover:text-dark transition-all"
+                  onClick={() => void handleSignIn('github')}
+                >
+                <Image src={github} alt="google" width="28" height="28" />
+                  Login with GitHub
+                </button>
+              </div>
             </div>
-            
-          </div>
+          </>
         )}
     </section>
   );
