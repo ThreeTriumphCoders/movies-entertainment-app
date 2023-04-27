2b1b75fc2067d525a3d2e0b8a5bdefb1eb4424f6
Stanislav Korchevskyi <stanislav.codes@gmail.com>
2023-04-27 23:56:40 +0300

    bookmarks redirect & signin page update

--- src/pages/bookmarks.tsx
+++ src/pages/bookmarks.tsx
@@ -1,5 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
-import { useEffect, useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { useEffect,useState } from 'react';
 import { MoviesList } from '~/components/MoviesList';
 import { useBookmarksContext } from '~/contexts/useBookmarks';
 import { type Category } from '~/types/Category.enum';
@@ -9,6 +11,18 @@ import { getMovie } from '~/utils/helpers';
 const BookmarksPage = () => {
   const { bookmarks } = useBookmarksContext();
   const [movies, setMovies] = useState<MoviesType>([]);
+  const { status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    const redirectToHomePage = async () => {
+      if (status === 'unauthenticated') {
+        await router.push('/auth/signin').catch(() => console.error('error occured'));
+      }
+    };
+
+    redirectToHomePage().catch((err) => console.error(err));
+  }, [status]);
 
   const { isError, refetch: loadMoreBookmarks } = useQuery({
     queryKey: ['bookmarked'],
