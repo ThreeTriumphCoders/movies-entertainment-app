55b6e01d7274e5ef0375868c74b143c9f1c38c84
Stanislav Korchevskyi <stanislav.codes@gmail.com>
2023-04-27 23:17:33 +0300

    Bookmarks Page Done

--- src/pages/bookmarks.tsx
+++ src/pages/bookmarks.tsx
@@ -1,29 +1,36 @@
-import { useEffect, useState } from "react";
-import { MoviesList } from "~/components/MoviesList";
-import { useBookmarksContext } from "~/contexts/useBookmarks";
-import { type Category } from "~/types/Category.enum";
-import { type MoviesType } from "~/types/Movie";
-import { getMovie } from "~/utils/helpers";
+import { useQuery } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
+import { MoviesList } from '~/components/MoviesList';
+import { useBookmarksContext } from '~/contexts/useBookmarks';
+import { type Category } from '~/types/Category.enum';
+import { type MoviesType } from '~/types/Movie';
+import { getMovie } from '~/utils/helpers';
 
 const BookmarksPage = () => {
   const { bookmarks } = useBookmarksContext();
-  const [movies, setMovies] = useState<MoviesType>([]); 
+  const [movies, setMovies] = useState<MoviesType>([]);
+
+  const { isError, refetch: loadMoreBookmarks } = useQuery({
+    queryKey: ['bookmarked'],
+    queryFn: () =>
+      Promise.all(
+        bookmarks.map(({ movieId, type }) =>
+          getMovie(movieId, type as Category),
+        ),
+      ),
+    onSuccess: (data => setMovies(data))
+  });
 
   useEffect(() => {
-    try {
-      Promise
-        .all(bookmarks.map(({ movieId, type }) => getMovie(movieId, type as Category)))
-        .then(setMovies)
-        .catch(console.log);
-      
-    } catch (err) {
-
-    }
+    void loadMoreBookmarks();
   }, [bookmarks]);
 
   return (
     <section>
-      <MoviesList title="Bookmarks" movies={movies} />
+      <MoviesList
+        title={isError ? 'No Bookmarks yet' : 'Bookmarks'}
+        movies={movies}
+      />
     </section>
   );
 };
