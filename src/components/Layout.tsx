0834421089a6cf3cd482b036338f704bb7124be8
Stanislav Korchevskyi <stanislav.codes@gmail.com>
2023-04-28 18:52:42 +0300

    Updated paddings and code style

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,26 +1,28 @@
-import React from "react";
-import { Navbar } from "./Navbar";
-import { Searchbar } from "./Searchbar";
-import { ScrollUpButton } from "./ScrollUpButton";
+import React from 'react';
+import { Navbar } from './Navbar';
+import { ScrollUpButton } from './ScrollUpButton';
+import { Searchbar } from './Searchbar';
 
 type Props = {
-  children : React.ReactNode,
-}
+  children: React.ReactNode;
+};
 
 export const Layout: React.FC<Props> = ({ children }) => {
   return (
-    <main 
+    <main
       className="
-        min-h-screen bg-dark font-body text-light lg:pl-40
-        px-4 sm:px-6 py-16 sm:pt-[116px] lg:pt-0
+        min-h-screen bg-dark px-4 pb-8 pt-16
+        font-body text-light selection:bg-primary 
+        selection:text-dark sm:px-6 sm:pt-[116px] lg:pl-40 lg:pt-0
       "
     >
       <Navbar />
+
       <Searchbar />
-      <div className="pb-10">
-        {children}
-      </div>
+
+      <div className="pb-10">{children}</div>
+
       <ScrollUpButton />
     </main>
   );
-}
+};
